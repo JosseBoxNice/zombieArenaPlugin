f9b3828be5ffbcdf9fe2eaa5821ab14694825a88
Joosua <joosua.virtanen08@gmail.com>
2026-09-23 21:02:40 +0300

    fix item dissapearing glitch and add dontDropXpOnDeath

--- src/main/java/io/github/josseboxnice/zombieArenaPlugin/zombieArenaPlugin.java
+++ src/main/java/io/github/josseboxnice/zombieArenaPlugin/zombieArenaPlugin.java
@@ -5,8 +5,14 @@ import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryType.SlotType;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.entity.Player;
+
 
 public class zombieArenaPlugin extends JavaPlugin implements Listener {
   @Override
@@ -17,10 +23,22 @@ public class zombieArenaPlugin extends JavaPlugin implements Listener {
   }
 
   @EventHandler
+  public void onInventoryClick(InventoryClickEvent event) {
+    if (this.getConfig().getBoolean("allowDroppingItems")) {
+      return;
+    }
+
+    if (event.getSlotType() == SlotType.OUTSIDE) {
+      event.setCancelled(true);
+    }
+  }
+
+  @EventHandler
   public void onPlayerDropItem(PlayerDropItemEvent event) {
     if (this.getConfig().getBoolean("allowDroppingItems")) {
       return;
     }
+    
     event.setCancelled(true);
   }
 
@@ -28,6 +46,9 @@ public class zombieArenaPlugin extends JavaPlugin implements Listener {
   public void onPlayerDead(PlayerDeathEvent event) {
     if (this.getConfig().getBoolean("clearItemsOnDeath")) {
       event.getDrops().clear();
+    }
+    if (!this.getConfig().getBoolean("dropXPOnDeath")) {
+      event.setShouldDropExperience(false);
     }
   } 
 }
